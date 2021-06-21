94fd2d2d781256b176ede50df25732c3b5e997f0
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-06-21 19:44:21 -0300

    added yml properties example

--- src/main/java/com/wagnerrmorais/springdependencyinjection/config/GreetingServiceConfig.java
+++ src/main/java/com/wagnerrmorais/springdependencyinjection/config/GreetingServiceConfig.java
@@ -9,7 +9,6 @@ import com.wagnerrmorais.springdependencyinjection.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
